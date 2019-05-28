67880c2a565e1508285f0b0fb41e3f494a0ed2fe
agjini <augustin.gjini@pm.me>
2019-05-28 18:42:40 +0200

    fix switch.sh

--- roles/core/templates/.bin/switch.sh
+++ roles/core/templates/.bin/switch.sh
@@ -8,11 +8,11 @@ isDocked() {
 if [ "$(isDocked)" = "1" ]; then
 
     echo "docked"
-    ~/.screenlayout/dock_home.sh
+    ~/.bin/dock_home.sh
 
 else
 
     echo "undocked"
-    ~/.screenlayout/laptop.sh
+    ~/.bin/laptop.sh
 
 fi;
