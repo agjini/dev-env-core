9b7b2aadcf9e870297a3ae78dd6c1b42210433ba
agjini <augustin.gjini@pm.me>
2019-06-11 09:27:20 +0200

    dock home

--- roles/core/templates/.bin/dock_home.sh
+++ roles/core/templates/.bin/dock_home.sh
@@ -2,7 +2,7 @@
 
 xrandr --output eDP1 --off
 
-xrandr --output DP1-1 --primary --auto
+xrandr --output HDMI1 --primary --auto
 
-xrandr --output DP1-2 --auto --right-of DP1-1
+xrandr --output DP2-2 --auto --right-of HDMI1
 
