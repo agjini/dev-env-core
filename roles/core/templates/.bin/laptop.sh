ed5a4dd77a3d806b77ed0c82fcff34675f9be200
Augustin Gjini <augustin.gjini@pm.me>
2019-09-06 20:49:06 +0200

    Update laptop.sh

--- roles/core/templates/.bin/laptop.sh
+++ roles/core/templates/.bin/laptop.sh
@@ -3,6 +3,10 @@
 xrandr --output eDP1 --auto --primary
 
 xrandr --output DP1-1 --off
-
 xrandr --output DP1-2 --off
 
+xrandr --output HDMI1 --off
+xrandr --output HDMI2 --off
+
+xrandr --output DP2-1 --off
+xrandr --output DP2-2 --off
