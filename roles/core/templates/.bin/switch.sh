22794bdb84c01e0735a62accca424eb66254afb7
Augustin Gjini <augustin.gjini@pm.me>
2020-04-24 09:22:40 +0200

    fix switch

--- roles/core/templates/.bin/switch.sh
+++ roles/core/templates/.bin/switch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 isDocked() {
-    boltctl list | grep disconnected
+    # Is here to force to rediscover available displays
+    # And check if there is other monitors that laptop connected
+    xrandr -q | grep -v "eDP1 connected" | grep " connected"
     echo $?
 }
 
-# Is here to force to rediscover available displays
-xrandr -q
-
-if [ "$(isDocked)" = "1" ]; then
+if [ "$(isDocked)" = "0" ]; then
 
     echo "docked"
     ~/.bin/dock_home.sh
