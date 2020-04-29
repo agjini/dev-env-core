ce0d61d51e48d01df2760b04fc5e92b4ec1c718b
Augustin Gjini <augustin.gjini@pm.me>
2020-04-29 18:31:25 +0200

    feat(i3): mod+shift+m switch to mono external screen and vice-versa

--- roles/core/templates/.bin/switch_mono_screen.sh
+++ roles/core/templates/.bin/switch_mono_screen.sh
@@ -8,27 +8,31 @@ getFirstExternalMonitor() {
     xrandr | grep " connected " | awk '{ print$1 }' | grep -v "eDP1" | head -1
 }
 
-if [ "$(getActiveMonitorsCount)" == 1 ]; then
+if [ "$(getActiveMonitorsCount)" > 0 ]; then
 
-    echo "Switching from mono screen to multi"
-    ~/.bin/switch.sh
+    if [ "$(getActiveMonitorsCount)" == 1 ]; then
 
-else
+        echo "Switching from mono screen to multi"
+        ~/.bin/switch.sh
 
-    echo "Switching from multi screen to mono"
+    else
 
-    xrandr --output eDP1 --off
+        echo "Switching from multi screen to mono"
 
-    xrandr --output DP1-1 --off
-    xrandr --output DP1-2 --off
+        xrandr --output eDP1 --off
 
-    xrandr --output HDMI-0 --off
-    xrandr --output HDMI-1 --off
-    xrandr --output HDMI-2 --off
+        xrandr --output DP1-1 --off
+        xrandr --output DP1-2 --off
 
-    xrandr --output DP2-1 --off
-    xrandr --output DP2-2 --off
+        xrandr --output HDMI-0 --off
+        xrandr --output HDMI-1 --off
+        xrandr --output HDMI-2 --off
 
-    xrandr --output $(getFirstExternalMonitor) --auto --primary
+        xrandr --output DP2-1 --off
+        xrandr --output DP2-2 --off
+
+        xrandr --output $(getFirstExternalMonitor) --auto --primary
+
+    fi;
 
 fi;
