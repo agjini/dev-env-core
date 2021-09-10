fa220d2ddc812c470c65268155d45c8f8c7938d5
Augustin Gjini <augustin.gjini@gmail.com>
2021-09-10 14:53:20 +0200

    fix dock-home with new display notation

--- roles/core/templates/.bin/switch_mono_screen.sh
+++ roles/core/templates/.bin/switch_mono_screen.sh
@@ -5,7 +5,7 @@ getActiveMonitorsCount() {
 }
 
 getFirstExternalMonitor() {
-    xrandr | grep " connected " | awk '{ print$1 }' | grep -v "eDP1" | head -1
+    xrandr | grep " connected " | awk '{ print$1 }' | grep -v "eDP-1" | head -1
 }
 
 if [ "$(getActiveMonitorsCount)" -gt 0 ]; then
@@ -19,17 +19,17 @@ if [ "$(getActiveMonitorsCount)" -gt 0 ]; then
 
         echo "Switching from multi screen to mono"
 
-        xrandr --output eDP1 --off
+        xrandr --output eDP-1 --off
 
-        xrandr --output DP1-1 --off
-        xrandr --output DP1-2 --off
+        xrandr --output DP-1-1 --off
+        xrandr --output DP-1-2 --off
 
         xrandr --output HDMI-0 --off
         xrandr --output HDMI-1 --off
         xrandr --output HDMI-2 --off
 
-        xrandr --output DP2-1 --off
-        xrandr --output DP2-2 --off
+        xrandr --output DP-2-1 --off
+        xrandr --output DP-2-2 --off
 
         xrandr --output $(getFirstExternalMonitor) --auto --primary
 
