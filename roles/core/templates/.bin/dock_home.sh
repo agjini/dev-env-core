aef2f390ec231f5ff0254b529cb7fd2e9f2940b6
Augustin Gjini <augustin.gjini@gmail.com>
2021-09-10 14:52:26 +0200

    fix dock-home with new display notation

--- roles/core/templates/.bin/dock_home.sh
+++ roles/core/templates/.bin/dock_home.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-xrandr --output eDP1 --off
+xrandr --output eDP-1 --off
 
 xrandr --output HDMI-0 --off
 xrandr --output HDMI-1 --off
 xrandr --output HDMI-2 --off
 
-xrandr --output DP1-1 --primary --auto
-xrandr --output DP1-2 --auto --right-of DP1-1
+xrandr --output DP-1-1 --primary --auto
+xrandr --output DP-1-2 --auto --right-of DP-1-1
 
--- roles/core/templates/.bin/switch.sh
+++ roles/core/templates/.bin/switch.sh
@@ -3,7 +3,7 @@
 isDocked() {
     # Is here to force to rediscover available displays
     # And check if there is other monitors that laptop connected
-    xrandr -q | grep -v "eDP1 connected" | grep " connected" > /dev/null 2>&1
+    xrandr -q | grep -v "eDP-1 connected" | grep " connected" > /dev/null 2>&1
     echo $?
 }
 
