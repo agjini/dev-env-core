5089ae4877063ec30c53f6435f110165d7b92e72
Augustin Gjini <augustin.gjini@gmail.com>
2021-09-17 16:02:35 +0200

    Display Ports display name have changed

--- roles/core/templates/.bin/laptop.sh
+++ roles/core/templates/.bin/laptop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-xrandr --output eDP1 --auto --primary
+xrandr --output eDP-1 --auto --primary
 
-xrandr --output DP1-1 --off
-xrandr --output DP1-2 --off
+xrandr --output DP-1-1 --off
+xrandr --output DP-1-2 --off
 
 xrandr --output HDMI-0 --off
 xrandr --output HDMI-1 --off
 xrandr --output HDMI-2 --off
 
-xrandr --output DP2-1 --off
-xrandr --output DP2-2 --off
+xrandr --output DP-2-1 --off
+xrandr --output DP-2-2 --off
