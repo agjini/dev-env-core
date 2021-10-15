8d22a71f6b5992f10a98d0217e16504e97419a50
Augustin Gjini <augustin.gjini@gmail.com>
2021-10-15 15:37:26 +0200

    fix display scripts

--- roles/core/templates/.bin/switch.sh
+++ roles/core/templates/.bin/switch.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
-getActiveMonitorsCount() {
-    xrandr --listactivemonitors | head -1 | awk '{ print$2 }'
+isConnected() {
+    local monitor=${1}
+    # Is here to force to rediscover available displays
+    # And check if there is other monitors that laptop connected
+    xrandr -q | grep "${monitor} connected" > /dev/null 2>&1
+    echo $?
 }
 
-getActiveMonitors() {
-    xrandr --listactivemonitors | tail -n+2 | awk '{ print$4 }'
+isActive() {
+    local monitor=${1}
+    xrandr --listactivemonitors | grep ${monitor} > /dev/null 2>&1
+    echo $?
 }
 
-if [ "$(getActiveMonitorsCount)" -gt 0 ]; then
+# check if naming is eDP-1 or eDP1
+checkDisplayName() {
+    xrandr -q | grep eDP1 > /dev/null 2>&1
+    echo $?
+}
 
-    if [ "$(getActiveMonitorsCount)" == 1 ]; then
+autoConfigure() {
+    local laptop=${1}
+    local monitor1=${2}
+    local monitor2=${3}
 
-        echo "Switching from mono screen to multi"
-        ~/.bin/switch.sh
-
+    if [ "$(isActive ${monitor1})" = "0" ] || [ "$(isConnected ${monitor1})" = "1" ]; then
+        xrandr --output ${laptop} --auto --primary
+        xrandr --output ${monitor1} --off
+        xrandr --output ${monitor2} --off
     else
-
-        echo "Switching from multi screen to mono"
-
-        local i=0
-        for value in $(getActiveMonitors)
-        do
-            if [ "${i}" = "0" ]; then
-                xrandr --output "${value}" --auto --primary
-            else
-                xrandr --output "${value}" --off
-            fi;
-            i=$(expr $i + 1)
-        done
-
-
+        xrandr --output ${monitor1} --auto --primary 
+        xrandr --output ${monitor2} --auto --right-of ${monitor1} 
+        xrandr --output ${laptop} --off
     fi;
+}
+
+
+if [ "$(checkDisplayName)" = "0" ]; then
+
+    autoConfigure eDP1 DP1-1 DP1-2
+
+else
+
+    autoConfigure eDP1-1 DP1-1 DP1-2
 
 fi;
